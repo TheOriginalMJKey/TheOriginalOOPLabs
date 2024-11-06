e0923a6f8936ec73547be2514d56adde7371a6d2
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-11-06 16:08:07 +0300

    Update triangle.cpp

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -1,25 +1,28 @@
 #include "triangle.h"
 #include <cmath>
 
-// Конструктор треугольника
-Triangle::Triangle(double sideLength) : sideLength(sideLength) {}
+template <typename T>
+Triangle<T>::Triangle(T sideLength) : sideLength(sideLength) {}
 
-// Вычисление геометрического центра треугольника
-std::pair<double, double> Triangle::getGeometricCenter() const {
-    return {0.0, sideLength / 3}; // Геометрический центр треугольника - центр высоты
+template <typename T>
+std::pair<T, T> Triangle<T>::getGeometricCenter() const {
+    return {0, sideLength / 3}; // Геометрический центр треугольника - центр высоты
 }
 
-// Вывод информации о треугольнике
-void Triangle::print(std::ostream& os) const {
+template <typename T>
+void Triangle<T>::print(std::ostream& os) const {
     os << "Triangle with side length: " << sideLength;
 }
 
-// Чтение данных треугольника из потока
-void Triangle::read(std::istream& is) {
+template <typename T>
+void Triangle<T>::read(std::istream& is) {
     is >> sideLength;
 }
 
-// Вычисление площади треугольника
-Triangle::operator double() const {
+template <typename T>
+Triangle<T>::operator double() const {
     return (std::sqrt(3) / 4) * std::pow(sideLength, 2);
 }
+
+template class Triangle<int>;
+template class Triangle<double>;
