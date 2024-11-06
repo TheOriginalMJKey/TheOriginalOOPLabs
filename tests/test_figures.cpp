92fcaf5087c77c7c5f3218a2877f059aa099fec5
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-11-06 16:09:21 +0300

    Update test_figures.cpp

--- tests/test_figures.cpp
+++ tests/test_figures.cpp
@@ -4,16 +4,16 @@
 #include "../include/triangle.h"
 
 TEST(FiguresTest, Hexagon) {
-    Hexagon hexagon(1.0);
+    Hexagon<int> hexagon(1);
     EXPECT_DOUBLE_EQ(static_cast<double>(hexagon), (3 * std::sqrt(3) / 2));
 }
 
 TEST(FiguresTest, Octagon) {
-    Octagon octagon(1.0);
+    Octagon<int> octagon(1);
     EXPECT_DOUBLE_EQ(static_cast<double>(octagon), 2 * (1 + std::sqrt(2)));
 }
 
 TEST(FiguresTest, Triangle) {
-    Triangle triangle(1.0);
+    Triangle<int> triangle(1);
     EXPECT_DOUBLE_EQ(static_cast<double>(triangle), (std::sqrt(3) / 4));
 }
