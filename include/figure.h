62a836d6c8b3e813384fb601baa1443bf971eace
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-11-06 15:34:57 +0300

    Create figure.h

---
+++ include/figure.h
@@ -0,0 +1,31 @@
+#ifndef FIGURE_H
+#define FIGURE_H
+
+#include <iostream>
+#include <utility>
+
+class Figure {
+public:
+    virtual ~Figure() = default;
+
+    virtual std::pair<double, double> getGeometricCenter() const = 0;
+    virtual void print(std::ostream& os) const = 0;
+    virtual void read(std::istream& is) = 0;
+    virtual operator double() const = 0;
+
+    Figure& operator=(const Figure& other) = default;
+    Figure& operator=(Figure&& other) noexcept = default;
+    bool operator==(const Figure& other) const = default;
+
+    friend std::ostream& operator<<(std::ostream& os, const Figure& figure) {
+        figure.print(os);
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Figure& figure) {
+        figure.read(is);
+        return is;
+    }
+};
+
+#endif // FIGURE_H
