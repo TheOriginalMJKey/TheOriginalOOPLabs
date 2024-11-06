e24bb5ed38abab1a20724df3060f227b8578c864
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-11-06 16:03:31 +0300

    Update hexagon.h

--- include/hexagon.h
+++ include/hexagon.h
@@ -3,17 +3,18 @@
 
 #include "figure.h"
 
-class Hexagon : public Figure {
+template <typename T>
+class Hexagon : public Figure<T> {
 public:
-    Hexagon(double sideLength);
+    Hexagon(T sideLength);
 
-    std::pair<double, double> getGeometricCenter() const override;
+    std::pair<T, T> getGeometricCenter() const override;
     void print(std::ostream& os) const override;
     void read(std::istream& is) override;
     operator double() const override;
 
 private:
-    double sideLength;
+    T sideLength;
 };
 
 #endif // HEXAGON_H
