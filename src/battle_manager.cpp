d8e9c618a6ea920313bf3968476c6250dd47862e
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-02 12:05:21 +0300

    Update battle_manager.cpp

--- src/battle_manager.cpp
+++ src/battle_manager.cpp
@@ -7,29 +7,13 @@ void BattleManager::addNPC(std::unique_ptr<NPC> npc) {
 }
 
 void BattleManager::startBattle(int range) {
-    for (auto& npc : npcs) {
-        for (auto& other : npcs) {
-            if (npc.get() != other.get() && npc->canFight(*other)) {
-                double distance = std::sqrt(std::pow(npc->getX() - other->getX(), 2) + std::pow(npc->getY() - other->getY(), 2));
-                if (distance <= range) {
-                    if (npc->fight(*other)) {
-                        std::string message = npc->getName() + " killed " + other->getName();
-                        for (auto& observer : observers) {
-                            observer->notify(message);
-                        }
-                        // Remove the defeated NPC
-                        other.reset();
-                    }
-                }
-            }
-        }
-    }
-    // Remove defeated NPCs from the list
-    npcs.erase(std::remove_if(npcs.begin(), npcs.end(), [](const std::unique_ptr<NPC>& npc) {
-        return npc == nullptr;
-    }), npcs.end());
+    startBattle(npcs, observers, range);
 }
 
 void BattleManager::addObserver(Observer* observer) {
     observers.push_back(observer);
+}
+
+const std::vector<std::unique_ptr<NPC>>& BattleManager::getNPCs() const {
+    return npcs;
 }
