a1af427d88ef4e4aab92b3d8bbab9f0d6530fc21
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-02 13:20:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,7 @@
-#include "battle_manager.h"
-#include "game_manager.h"
-#include "observer.h"
-#include <iostream>
-#include <thread>
+#include "game.h"
 
 int main() {
-    BattleManager battleManager;
-    FileObserver fileObserver;
-    ConsoleObserver consoleObserver;
-
-    battleManager.addObserver(&fileObserver);
-    battleManager.addObserver(&consoleObserver);
-
-    GameManager gameManager(battleManager);
-    gameManager.startGame();
-
-    std::this_thread::sleep_for(std::chrono::seconds(30));
-
-    std::lock_guard<std::shared_mutex> lock(gameManager.mutex);
-    std::cout << "Survivors:" << std::endl;
-    for (const auto& npc : battleManager.getNPCs()) {
-        if (npc->isAlive()) {
-            std::cout << npc->getName() << " (" << npc->getX() << ", " << npc->getY() << ")" << std::endl;
-        }
-    }
-
+    Game game(100, 30);
+    game.start();
     return 0;
 }
