29ce09823664dc207e672e4ef0b2f0fc34c1ae06
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-12 17:10:33 +0300

    Update battle_manager.h

--- include/battle_manager.h
+++ include/battle_manager.h
@@ -5,6 +5,7 @@
 #include "observer.h"
 #include <vector>
 #include <memory>
+#include <iostream>
 
 class BattleManager {
 public:
@@ -12,10 +13,13 @@ public:
     void startBattle(int range);
     void addObserver(Observer* observer);
     const std::vector<std::unique_ptr<NPC>>& getNPCs() const;
+    void printNPCs() const;
 
 private:
     std::vector<std::unique_ptr<NPC>> npcs;
     std::vector<Observer*> observers;
 };
+
+void startBattle(std::vector<std::unique_ptr<NPC>>& npcs, std::vector<Observer*>& observers, int range);
 
 #endif // BATTLE_MANAGER_H
