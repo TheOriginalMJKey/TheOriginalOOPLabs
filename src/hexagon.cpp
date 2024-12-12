fb698cda8a8ff3e65df5f2a29f264fc07a8554e6
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-12 16:56:25 +0300

    Update hexagon.cpp

--- src/hexagon.cpp
+++ src/hexagon.cpp
@@ -1,25 +1,29 @@
 #include "hexagon.h"
 #include <cmath>
 
-// Конструктор шестиугольника
 Hexagon::Hexagon(double sideLength) : sideLength(sideLength) {}
 
-// Вычисление геометрического центра шестиугольника
+Hexagon::Hexagon(const Hexagon& other) : sideLength(other.sideLength) {}
+
+Hexagon& Hexagon::operator=(const Hexagon& other) {
+    if (this != &other) {
+        sideLength = other.sideLength;
+    }
+    return *this;
+}
+
 std::pair<double, double> Hexagon::getGeometricCenter() const {
     return {0.0, 0.0}; // Геометрический центр шестиугольника - центр
 }
 
-// Вывод информации о шестиугольнике
 void Hexagon::print(std::ostream& os) const {
     os << "Hexagon with side length: " << sideLength;
 }
 
-// Чтение данных шестиугольника из потока
 void Hexagon::read(std::istream& is) {
     is >> sideLength;
 }
 
-// Вычисление площади шестиугольника
 Hexagon::operator double() const {
     return (3 * std::sqrt(3) / 2) * std::pow(sideLength, 2);
 }
