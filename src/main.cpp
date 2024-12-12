8c7522c031267418bdbcfae770137b607c1c39a4
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-12 17:29:20 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include "npc_factory.h"
 #include "battle_manager.h"
+#include "npc_factory.h"
 #include "file_manager.h"
 #include "observer.h"
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main() {
     BattleManager battleManager;
@@ -12,13 +14,13 @@ int main() {
     battleManager.addObserver(&fileObserver);
     battleManager.addObserver(&consoleObserver);
 
-    battleManager.addNPC(NPCFactory::createNPC("Orc", "Orc1", 100, 100));
-    battleManager.addNPC(NPCFactory::createNPC("Squirrel", "Squirrel1", 200, 200));
-    battleManager.addNPC(NPCFactory::createNPC("Bear", "Bear1", 300, 300));
+    battleManager.startGame();
 
-    FileManager::saveNPCs(battleManager.getNPCs(), "npcs.txt");
+    std::this_thread::sleep_for(std::chrono::seconds(30));
+    battleManager.stopGame();
+    battleManager.joinThreads();
 
-    battleManager.startBattle(150);
+    battleManager.printNPCs();
 
     return 0;
 }
