1b11da444fb50f14fa140f3209dc1f41c70468f3
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-12 16:57:16 +0300

    Update triangle.cpp

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -1,25 +1,29 @@
 #include "triangle.h"
 #include <cmath>
 
-// Конструктор треугольника
 Triangle::Triangle(double sideLength) : sideLength(sideLength) {}
 
-// Вычисление геометрического центра треугольника
+Triangle::Triangle(const Triangle& other) : sideLength(other.sideLength) {}
+
+Triangle& Triangle::operator=(const Triangle& other) {
+    if (this != &other) {
+        sideLength = other.sideLength;
+    }
+    return *this;
+}
+
 std::pair<double, double> Triangle::getGeometricCenter() const {
     return {0.0, sideLength / 3}; // Геометрический центр треугольника - центр высоты
 }
 
-// Вывод информации о треугольнике
 void Triangle::print(std::ostream& os) const {
     os << "Triangle with side length: " << sideLength;
 }
 
-// Чтение данных треугольника из потока
 void Triangle::read(std::istream& is) {
     is >> sideLength;
 }
 
-// Вычисление площади треугольника
 Triangle::operator double() const {
     return (std::sqrt(3) / 4) * std::pow(sideLength, 2);
 }
