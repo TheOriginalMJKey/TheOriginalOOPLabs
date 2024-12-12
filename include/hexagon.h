e844696d06a45e40b2b39101f38d796aa29c5920
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-12 17:01:40 +0300

    Update hexagon.h

--- include/hexagon.h
+++ include/hexagon.h
@@ -2,16 +2,28 @@
 #define HEXAGON_H
 
 #include "figure.h"
+#include <cmath>
 
 template <typename T>
 class Hexagon : public Figure<T> {
 public:
-    Hexagon(T sideLength);
+    Hexagon(T sideLength) : sideLength(sideLength) {}
 
-    std::pair<T, T> getGeometricCenter() const override;
-    void print(std::ostream& os) const override;
-    void read(std::istream& is) override;
-    operator double() const override;
+    std::pair<T, T> getGeometricCenter() const override {
+        return {0, 0}; // Геометрический центр шестиугольника - центр
+    }
+
+    void print(std::ostream& os) const override {
+        os << "Hexagon with side length: " << sideLength;
+    }
+
+    void read(std::istream& is) override {
+        is >> sideLength;
+    }
+
+    operator double() const override {
+        return (3 * std::sqrt(3) / 2) * std::pow(sideLength, 2);
+    }
 
 private:
     T sideLength;
