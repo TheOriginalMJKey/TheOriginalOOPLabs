852d033a2812a0a7aadc813429ce0d8817244f38
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-12 17:02:27 +0300

    Update triangle.h

--- include/triangle.h
+++ include/triangle.h
@@ -2,16 +2,28 @@
 #define TRIANGLE_H
 
 #include "figure.h"
+#include <cmath>
 
 template <typename T>
 class Triangle : public Figure<T> {
 public:
-    Triangle(T sideLength);
+    Triangle(T sideLength) : sideLength(sideLength) {}
 
-    std::pair<T, T> getGeometricCenter() const override;
-    void print(std::ostream& os) const override;
-    void read(std::istream& is) override;
-    operator double() const override;
+    std::pair<T, T> getGeometricCenter() const override {
+        return {0, sideLength / 3}; // Геометрический центр треугольника - центр высоты
+    }
+
+    void print(std::ostream& os) const override {
+        os << "Triangle with side length: " << sideLength;
+    }
+
+    void read(std::istream& is) override {
+        is >> sideLength;
+    }
+
+    operator double() const override {
+        return (std::sqrt(3) / 4) * std::pow(sideLength, 2);
+    }
 
 private:
     T sideLength;
