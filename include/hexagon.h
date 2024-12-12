9573d800d7f69380d9dfb64cdea1e5c3ee0b08a1
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-12 16:54:06 +0300

    Update hexagon.h

--- include/hexagon.h
+++ include/hexagon.h
@@ -6,6 +6,10 @@
 class Hexagon : public Figure {
 public:
     Hexagon(double sideLength);
+    Hexagon(const Hexagon& other);
+    Hexagon(Hexagon&& other) noexcept = default;
+    Hexagon& operator=(const Hexagon& other);
+    Hexagon& operator=(Hexagon&& other) noexcept = default;
 
     std::pair<double, double> getGeometricCenter() const override;
     void print(std::ostream& os) const override;
