7d8805bd7dfa3771ebc5bd4fad8459831cbe4755
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-12 17:00:02 +0300

    Update hexagon.cpp

--- src/hexagon.cpp
+++ src/hexagon.cpp
@@ -1,28 +1,33 @@
-#include "hexagon.h"
+#ifndef HEXAGON_H
+#define HEXAGON_H
+
+#include "figure.h"
 #include <cmath>
+#include <memory>
 
 template <typename T>
-Hexagon<T>::Hexagon(T sideLength) : sideLength(sideLength) {}
+class Hexagon : public Figure<T> {
+public:
+    Hexagon(T sideLength) : sideLength(sideLength) {}
 
-template <typename T>
-std::pair<T, T> Hexagon<T>::getGeometricCenter() const {
-    return {0, 0}; // Геометрический центр шестиугольника - центр
-}
+    std::pair<T, T> getGeometricCenter() const override {
+        return {0, 0}; 
+    }
 
-template <typename T>
-void Hexagon<T>::print(std::ostream& os) const {
-    os << "Hexagon with side length: " << sideLength;
-}
+    void print(std::ostream& os) const override {
+        os << "Hexagon with side length: " << sideLength;
+    }
 
-template <typename T>
-void Hexagon<T>::read(std::istream& is) {
-    is >> sideLength;
-}
+    void read(std::istream& is) override {
+        is >> sideLength;
+    }
 
-template <typename T>
-Hexagon<T>::operator double() const {
-    return (3 * std::sqrt(3) / 2) * std::pow(sideLength, 2);
-}
+    operator double() const override {
+        return (3 * std::sqrt(3) / 2) * std::pow(sideLength, 2);
+    }
 
-template class Hexagon<int>;
-template class Hexagon<double>;
+private:
+    T sideLength;
+};
+
+#endif // HEXAGON_H
