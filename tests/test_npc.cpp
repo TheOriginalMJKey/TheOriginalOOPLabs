3611858b93b15b055c7167c73aaea509bb1dae37
TheOriginalMJKey <141678058+TheOriginalMJKey@users.noreply.github.com>
2024-12-12 17:19:15 +0300

    Update test_npc.cpp

--- tests/test_npc.cpp
+++ tests/test_npc.cpp
@@ -1,27 +1,49 @@
 #include <gtest/gtest.h>
 #include "npc.h"
-#include "npc_factory.h"
+#include "npc_visitor.h"
 
-TEST(NPCFactoryTest, CreateOrc) {
-    auto orc = NPCFactory::createNPC("Orc", "Orc1", 100, 100);
-    ASSERT_EQ(orc->getType(), "Orc");
-    ASSERT_EQ(orc->getName(), "Orc1");
-    ASSERT_EQ(orc->getX(), 100);
-    ASSERT_EQ(orc->getY(), 100);
+class MockVisitor : public NPCVisitor {
+public:
+    void visit(Orc& orc) override {
+        visitedOrc = true;
+    }
+
+    void visit(Squirrel& squirrel) override {
+        visitedSquirrel = true;
+    }
+
+    void visit(Bear& bear) override {
+        visitedBear = true;
+    }
+
+    bool visitedOrc = false;
+    bool visitedSquirrel = false;
+    bool visitedBear = false;
+};
+
+TEST(NPCVisitorTest, VisitOrc) {
+    MockVisitor visitor;
+    Orc orc("Orc1", 100, 100);
+    orc.accept(visitor);
+    ASSERT_TRUE(visitor.visitedOrc);
+    ASSERT_FALSE(visitor.visitedSquirrel);
+    ASSERT_FALSE(visitor.visitedBear);
 }
 
-TEST(NPCFactoryTest, CreateSquirrel) {
-    auto squirrel = NPCFactory::createNPC("Squirrel", "Squirrel1", 200, 200);
-    ASSERT_EQ(squirrel->getType(), "Squirrel");
-    ASSERT_EQ(squirrel->getName(), "Squirrel1");
-    ASSERT_EQ(squirrel->getX(), 200);
-    ASSERT_EQ(squirrel->getY(), 200);
+TEST(NPCVisitorTest, VisitSquirrel) {
+    MockVisitor visitor;
+    Squirrel squirrel("Squirrel1", 200, 200);
+    squirrel.accept(visitor);
+    ASSERT_FALSE(visitor.visitedOrc);
+    ASSERT_TRUE(visitor.visitedSquirrel);
+    ASSERT_FALSE(visitor.visitedBear);
 }
 
-TEST(NPCFactoryTest, CreateBear) {
-    auto bear = NPCFactory::createNPC("Bear", "Bear1", 300, 300);
-    ASSERT_EQ(bear->getType(), "Bear");
-    ASSERT_EQ(bear->getName(), "Bear1");
-    ASSERT_EQ(bear->getX(), 300);
-    ASSERT_EQ(bear->getY(), 300);
+TEST(NPCVisitorTest, VisitBear) {
+    MockVisitor visitor;
+    Bear bear("Bear1", 300, 300);
+    bear.accept(visitor);
+    ASSERT_FALSE(visitor.visitedOrc);
+    ASSERT_FALSE(visitor.visitedSquirrel);
+    ASSERT_TRUE(visitor.visitedBear);
 }
